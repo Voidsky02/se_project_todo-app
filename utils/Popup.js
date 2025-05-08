be82582964ce4106e5933dded79240819ec9ab2a
voidsky <voidsky02@gmail.com>
2025-05-08 01:12:48 -0700

    add escape key method for closing modals

--- utils/Popup.js
+++ utils/Popup.js
@@ -1,6 +1,7 @@
 class Popup {
     constructor(popupSelector) {
-        this._popupSelector = popupSelector;
+        // thinks its better to do querySelector here
+        this._popupSelector = document.querySelector(popupSelector);
     }
 
     open() {
@@ -9,21 +10,38 @@ class Popup {
         handlers in index.js. */
 
         this._popupSelector.classList.add("popup_visible");
+
+        // use handleEscapeClose() to add escape key func
+        this._handleEscapeClose;
     }
 
     close() {
         // close the popup
         this._popupSelector.classList.remove("popup_visible");
+
+        // remove handleEscapeKey so it isnt checking keys all the time
     }
 
     _handleEscapeClose() {
         /* stores the logic for closing the popup by pressing 
         the Escape key. */
-        this._popupSelector.addEventListner("keydown", (event) => {
+
+        // going to call this on opening modals
+        this._popupSelector.addEventListener("keydown", isKeyEscape);
+
+        // func for checking if key is Escape and what to do with it
+        const isKeyEscape = (event) => {
             if (event.key === "Escape") {
                 this.close();
+                removeEscapeKeyListener();
             }
-        })
+        }
+
+        // create func for removing Escape key event listener, put 
+        // this in the isKeyEscape func
+        const removeEscapeKeyListener = () => {
+            this._popupSelector.removeEventListener("keydown", isKeyEscape);
+        }
     }
 
     setEventListeners() {
@@ -31,7 +49,6 @@ class Popup {
         The modal window should also close when users click on the 
         shaded area around the form. */
         
-        // below might not work because this._popupSelector is probably a string (will fix later)
         this._popupSelector.querySelector(".popup__close").addEventListner("click", this.close);
     }
 }
